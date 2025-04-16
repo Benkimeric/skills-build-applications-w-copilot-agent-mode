657f134590743ae1650e617f3d2283d74b596d7d
Eric <benkimeric@gmail.com>
2025-04-16 09:18:01 +0000

    Add initial Django project structure with models, views, serializers, and settings

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,44 @@
+from django.core.management.base import BaseCommand
+from octofit_tracker.models import User, Team, Activity, Leaderboard, Workout
+from bson import ObjectId
+from datetime import timedelta
+
+class Command(BaseCommand):
+    help = 'Populate the database with test data for users, teams, activities, leaderboard, and workouts'
+
+    def handle(self, *args, **kwargs):
+        # Create users
+        users = [
+            User(_id=ObjectId(), username='thundergod', email='thundergod@mhigh.edu', password='password1'),
+            User(_id=ObjectId(), username='metalgeek', email='metalgeek@mhigh.edu', password='password2'),
+            User(_id=ObjectId(), username='zerocool', email='zerocool@mhigh.edu', password='password3'),
+        ]
+        User.objects.bulk_create(users)
+
+        # Create teams
+        team = Team(_id=ObjectId(), name='Blue Team')
+        team.save()
+        team.members.add(*users)
+
+        # Create activities
+        activities = [
+            Activity(_id=ObjectId(), user=users[0], activity_type='Cycling', duration=timedelta(hours=1)),
+            Activity(_id=ObjectId(), user=users[1], activity_type='Running', duration=timedelta(hours=2)),
+        ]
+        Activity.objects.bulk_create(activities)
+
+        # Create leaderboard entries
+        leaderboard_entries = [
+            Leaderboard(_id=ObjectId(), user=users[0], score=100),
+            Leaderboard(_id=ObjectId(), user=users[1], score=90),
+        ]
+        Leaderboard.objects.bulk_create(leaderboard_entries)
+
+        # Create workouts
+        workouts = [
+            Workout(_id=ObjectId(), name='Cycling Training', description='Training for a cycling event'),
+            Workout(_id=ObjectId(), name='Running Training', description='Training for a marathon'),
+        ]
+        Workout.objects.bulk_create(workouts)
+
+        self.stdout.write(self.style.SUCCESS('Successfully populated the database with test data.'))
